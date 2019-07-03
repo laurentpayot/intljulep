71be99ba15bd2cbbd0031e7ddcba2dec5851d5e0
Laurent Payot <laurent.payot@publiberty.com>
2019-07-03 13:48:55 +0200

    removed unneeded parentheses

--- index.js
+++ index.js
@@ -1,13 +1,13 @@
 function i18n(path, data) {
-  let val = path.split('.').reduce(((acc, curr) => acc[curr]), i18n.locales[i18n.locale])
+  let val = path.split('.').reduce((acc, curr) => acc[curr], i18n.locales[i18n.locale])
   if (Array.isArray(val)) {
     return val[data - 1] || val[val.length - 1]
   } else {
     return val ? val
-      .replace(/{[^@}]+}/g, (s) => data[s.slice(1, -1)])
-      .replace(/{@[^}]+}/g, (s) => {
+      .replace(/{[^@}]+}/g, s => data[s.slice(1, -1)])
+      .replace(/{@[^}]+}/g, s => {
         let n = null
-        let subpath = s.slice(2, -1).replace(/\(.*\)/, (argStr) => {
+        let subpath = s.slice(2, -1).replace(/\(.*\)/, argStr => {
           n = data[argStr.slice(1, -1)]
           return '' // removing eventual argument list string
         })
@@ -19,6 +19,6 @@ function i18n(path, data) {
 
 i18n.locales = {}
 i18n.addLocale = (lang, dic) => i18n.locales[lang] = dic
-i18n.setLocale = (lang) => i18n.locale = lang
+i18n.setLocale = lang => i18n.locale = lang
 
-module.exports = {default: i18n, i18n}
+module.exports = { default: i18n, i18n }
